6a79afad37b8d1326f399cff364031183cb2392a
EOussama <oussama_gtacj@live.fr>
2023-03-15 17:14:18 +0100

    Java starter project

---
+++ README.md
@@ -0,0 +1,3 @@
+# Selenium PW for Systémes SI
+
+This is a starter project for using Selenium with Java. It opens the webpage "http://www.tutorialsninja.com/demo/" and performs some basic actions using Selenium.
---
+++ src/main/java/est/systems_si/selenium/App.java
@@ -0,0 +1,13 @@
+package est.systems_si.selenium;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
