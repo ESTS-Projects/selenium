8880b3fe44d3e9f4b4a2e94ecaf4d947100cfc6a
EOussama <oussama_gtacj@live.fr>
2023-03-15 17:28:55 +0100

    Added starter code

--- src/main/java/est/systems_si/selenium/App.java
+++ src/main/java/est/systems_si/selenium/App.java
@@ -1,13 +1,28 @@
 package est.systems_si.selenium;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 /**
- * Hello world!
- *
+ * Selenium demo for systémes SI
  */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        // Set the path to the ChromeDriver executable
+        System.setProperty("webdriver.chrome.driver", "/path/to/chromedriver");
+
+        // Create a new ChromeDriver instance
+        WebDriver driver = new ChromeDriver();
+
+        // Navigate to Google
+        driver.get("https://www.google.com/");
+
+        // Print the page title
+        System.out.println("Page title: " + driver.getTitle());
+
+        // Close the browser
+        driver.quit();
     }
 }
