d5a3a79d4d977ca40dcb2ffeabbc8bf1f2792f99
EOussama <oussama_gtacj@live.fr>
2023-03-19 16:25:43 +0000

    Tweaks

--- src/main/java/est/systems_si/selenium/App.java
+++ src/main/java/est/systems_si/selenium/App.java
@@ -1,24 +1,16 @@
 package est.systems_si.selenium;
 
-import java.util.Random;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 
 
 public class App {
-    static final int CONFIG_TIMEOUT = 300;
-    static final String CONFIG_URL = "http://www.tutorialsninja.com/demo";
-    static final String CONFIG_DRIVER = "C:\\Users\\EOussama\\Documents\\études\\modules\\management des sI\\tps\\selenium\\drivers\\msedgedriver.exe";
-    static final String CONFIG_EMAIL = "my_email_" + Integer.toString(new Random().nextInt(1000)) + "@gmail.com";
-    static final String CONFIG_PASSWORD = "my_password";
-
     public static void main(String[] args) throws InterruptedException {
-        System.setProperty("webdriver.edge.driver", CONFIG_DRIVER);
+        System.setProperty("webdriver.edge.driver", Config.driver);
         WebDriver page = new EdgeDriver();
         
-        page.get(CONFIG_URL);
+        page.get(Config.Url);
         automateRegister(page);
         automateLogout(page);
         automateLogin(page);
@@ -31,12 +23,12 @@ public class App {
     private static void automateRegister(WebDriver page) throws InterruptedException {
         click(page, "//*[@id=\"top-links\"]/ul/li[2]/a");
         click(page, "//*[@id=\"top-links\"]/ul/li[2]/ul/li[1]/a");
-        type(page, "//*[@id=\"input-firstname\"]", "my_first_name");
-        type(page, "//*[@id=\"input-lastname\"]", "my_last_name");
-        type(page, "//*[@id=\"input-email\"]", CONFIG_EMAIL);
-        type(page, "//*[@id=\"input-telephone\"]", "0656895632");
-        type(page, "//*[@id=\"input-password\"]", CONFIG_PASSWORD);
-        type(page, "//*[@id=\"input-confirm\"]", CONFIG_PASSWORD);
+        type(page, "//*[@id=\"input-firstname\"]", Config.firstName);
+        type(page, "//*[@id=\"input-lastname\"]", Config.lastName);
+        type(page, "//*[@id=\"input-email\"]", Config.email);
+        type(page, "//*[@id=\"input-telephone\"]", Config.phoneNumber);
+        type(page, "//*[@id=\"input-password\"]", Config.password);
+        type(page, "//*[@id=\"input-confirm\"]", Config.password);
         click(page, "//*[@id=\"content\"]/form/div/div/input[1]");
         click(page, "//*[@id=\"content\"]/form/div/div/input[2]");
         click(page, "//*[@id=\"content\"]/div/div/a");
@@ -51,8 +43,8 @@ public class App {
     private static void automateLogin(WebDriver page) throws InterruptedException {
         click(page, "//*[@id=\"top-links\"]/ul/li[2]/a");
         click(page, "//*[@id=\"top-links\"]/ul/li[2]/ul/li[2]/a");
-        type(page, "//*[@id=\"input-email\"]", CONFIG_EMAIL);
-        type(page, "//*[@id=\"input-password\"]", CONFIG_PASSWORD);
+        type(page, "//*[@id=\"input-email\"]", Config.email);
+        type(page, "//*[@id=\"input-password\"]", Config.password);
         click(page, "//*[@id=\"content\"]/div/div[2]/div/form/input");
     }
 
@@ -66,11 +58,11 @@ public class App {
 
     private static void click(WebDriver page, String xpath) throws InterruptedException {
         page.findElement(By.xpath(xpath)).click();
-        Thread.sleep(CONFIG_TIMEOUT);
+        Thread.sleep(Config.timeout);
     }
 
     private static void type(WebDriver page, String xpath, String content) throws InterruptedException {
         page.findElement(By.xpath(xpath)).sendKeys(content);
-        Thread.sleep(CONFIG_TIMEOUT);
+        Thread.sleep(Config.timeout);
     }
 }
---
+++ src/main/java/est/systems_si/selenium/Config.java
@@ -0,0 +1,14 @@
+package est.systems_si.selenium;
+
+import java.util.Random;
+
+public class Config {
+    static final int timeout = 200;
+    static final String password = "my_password";
+    static final String lastName = "my_last_name";
+    static final String phoneNumber = "0656895632";
+    static final String firstName = "my_first_name";
+    static final String Url = "http://www.tutorialsninja.com/demo";
+    static final String email = "my_email_" + Integer.toString(new Random().nextInt(1000)) + "@gmail.com";
+    static final String driver = "C:\\Users\\EOussama\\Documents\\études\\modules\\management des sI\\tps\\selenium\\drivers\\msedgedriver.exe";
+}
